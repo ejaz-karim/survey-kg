0bc5708b63ba8865a7fcf2abbde8e7fce5211104
ejaz-karim <ejaz.karim@kcl.ac.uk>
2023-04-11 15:05:44 +0100

    Add filter_requests_output.py

---
+++ filter_requests_output.py
@@ -0,0 +1,10 @@
+dbpedia = "http://dbpedia.org/resource/"
+
+with open("requests_output.txt", "r", encoding="utf-8") as f, open("requests_output_filtered.txt", "w", encoding="utf-8") as g:
+    lines_seen = set()
+    for line in f:
+        split = line.split('"')
+        for word in split:
+            if dbpedia in word and word not in lines_seen:
+                g.write(word + "\n")
+                lines_seen.add(word)
