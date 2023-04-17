f707d1657929fefb08f808f610ac6abbe0326d37
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2023-04-17 11:22:56 +0100

    Update data_iteration.py

--- data_iteration.py
+++ data_iteration.py
@@ -3,13 +3,29 @@ import filter_requests_output
 
 with open("seeAlso.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
+    t = tuple(line.strip() for line in lines)
 
-tuple = tuple(line.strip() for line in lines)
-
-with open("survey7.ttl", "r", encoding="utf-8") as g:
-    seeAlso_count = 0
-    for word in tuple:
-        requests_api.db_api(word)
-        filter_requests_output.iteration()
-        
-
+    with open("survey7.ttl", "r", encoding="utf-8") as g:
+        with open("requests_output_filtered.txt", "w+", encoding="utf-8") as h:
+            with open("survey7_modified.ttl", "w+", encoding="utf-8") as j:
+                with open("requests_output.txt", "w+", encoding="utf-8") as k:
+                    h.truncate(0)
+                    k.truncate(0)
+                    j.truncate(0)
+                    seealso_count = 0
+                    for line in g:
+                        if "seeAlso" in line:
+                            seealso_count += 1
+                            if seealso_count <= len(t):
+                                word = t[seealso_count-1]
+                                requests_api.db_api(word)
+                                filter_requests_output.iteration()
+                                h.seek(0)
+                                j.write(line)
+                                j.write(h.read())
+                            else:
+                                j.write(line)
+                        else:
+                            j.write(line)
+                    h.truncate(0)
+                    k.truncate(0)
