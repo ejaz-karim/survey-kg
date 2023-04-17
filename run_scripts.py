c8547e51dc7ae4cb9708f4ff09f94248f6d8b2c2
ejaz-karim <78738965+ejaz-karim@users.noreply.github.com>
2023-04-17 19:45:18 +0100

    Update run_scripts.py

--- run_scripts.py
+++ run_scripts.py
@@ -1,14 +1,33 @@
-import os
+import subprocess
 import time
 
-start_time = time.time()
+def time_api():
+    start_time = time.time()
 
-os.system('java -jar sparql-anything-0.8.1.jar -q query7.sparql -o survey7.ttl')
+    subprocess.run(['java', '-jar', 'sparql-anything-0.8.1.jar', '-q', 'query7.sparql', '-o', 'survey7.ttl'], check=True)
 
-os.system('python data_extraction.py')
-os.system('python data_iteration.py')
+    subprocess.run(['python', 'data_extraction.py'], check=True)
+    subprocess.run(['python', 'data_iteration.py'], check=True)
 
-end_time = time.time()
-elapsed_time = end_time - start_time
+    end_time = time.time()
+    elapsed_time = end_time - start_time
 
-print('Execution time: {:.2f} seconds'.format(elapsed_time))
+    print('Execution time: {:.2f} seconds'.format(elapsed_time))
+
+def dbr_count():
+    with open("survey7.ttl", 'r', encoding="utf-8") as file1, open("survey7_modified.ttl", 'r', encoding="utf-8") as file2:
+        lines1 = file1.readlines()
+        lines2 = file2.readlines()
+
+    new_lines = [line for line in lines2 if line not in lines1]
+    count = 0
+
+    for line in new_lines:
+        count = count + 1
+    print("dbr:hasResource appears " + str(count) + " times, the expected number is 809")
+
+def main():
+    time_api()
+    dbr_count()
+if __name__ == '__main__':
+    main()
